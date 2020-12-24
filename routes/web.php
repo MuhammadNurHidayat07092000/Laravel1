b2e6c1d4f913040fa159d630f1ff5406dfad2fd6
muhammadnurhidayat07092000 <yyt08536@gmailcom>
2020-12-24 09:16:55 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foo', function () {
+    return 'Hello World';
+});
+
+Route::redirect('/here', '/foo');
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return $name;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        // Matches The "/admin/users" URL
+        return "admin > user";
+    });
+});
